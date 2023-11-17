875f14283fa0ff949537df6a87536159725426eb
CAPACITACION TI <CAPTI@cfe.mx>
2023-11-17 13:54:48 -0600

    Elimina bootstrap y lo enlaza desde un enlace externo, con la finalidad de reducir el peso del proyecto

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <!-- Global CSS -->
     <link href="resources/css/styles_global.css" rel="stylesheet">
     <!-- Bootstrap link -->
-    <link rel="stylesheet" href="resources/bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
